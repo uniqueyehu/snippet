ddd85207fcc7450b5ed18eed86a9bbd07a6c6c13
Ye Hu <yehuwuhan@gmail.com>
2017-09-10 23:21:20 +0800

    add use_lambda.cpp

---
+++ cpp/use_lambda.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// 使用三个参数版本的sort函数
+// sort函数的第三个参数是一个为谓词
+// 一元谓词：单一参数；二元谓词：两个参数
+// 将isShorter函数作为谓词传递给sort
+
+// 比较函数，用来按长度排序单词
+bool isShorter(const string &s1, const string &s2)
+{
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
