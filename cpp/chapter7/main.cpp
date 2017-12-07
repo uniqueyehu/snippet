6cac93a431c2b9b3f1d2214f81d05444499f1db1
Ye Hu <yehuwuhan@gmail.com>
2017-12-07 19:44:01 +0800

    updata chapter7

--- cpp/chapter7/main.cpp
+++ cpp/chapter7/main.cpp
@@ -1,64 +1,33 @@
-﻿#include "chapter_7.h"
+﻿/****************************************************************************
+ **
+ ** This file is part of the cprimer_7 project.
+ ** Copyright (C) 2017 yehu <yehuwuhan@gmail.com>.
+ ** All rights reserved.
+ **
+ ** Author      : Ye Hu
+ ** Version     : v1.0
+ ** Description : 类的定义、类的成员函数、非成员函数、类的构造函数
+ **               访问控制、友元、类的静态成员
+ **
+****************************************************************************/
+
+#include "chapter_7.h"
 
 using namespace std;
 
-/**********************************************************/
-int fact(int val)
-{
-    int ret = 1;
-    while (val > 1)
-        ret *= val--;
-    return ret;
-}
-
-void test_1()
-{
-    int j = fact(5);
-    cout << "5! is " << j << endl;
-}
-
-//使用指针形参交换两个整数的值
-//指针形参为值传递，能改变指针所指向的值，但无法改变指针的值
-void switch_value(int *a, int *b)
-{
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-
-    a = 0;//这里改变传入的指针的值，只改变了a的局部拷贝，实参本身并没有被改变
-}
-
-void test_2()
-{
-    int a = 1, b = 2;
-    switch_value(&a, &b);
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-}
-
-//i是传给reset函数的对象的另一个名字
-//采用引用传值：
-//1、避免拷贝
-//2、返回额外信息
-void reset(int &i)
-{
-    i = 0;
-}
-
-
-void test_3()
-{
-    int i = 3;
-    reset(i);
-    cout << "i = " << i << endl;
-}
-/**********************************************************/
-
-
-
 int main(int argc, char *argv[])
 {
-//    test_3();
+    Screen myscreen;
+    char ch = myscreen.get();
+    cout << ch << endl;
+    ch = myscreen.get(0, 0);
+    cout << ch << endl;
+    // 光标移到(4,0)，设置该位置为#
+    myscreen.move(4,0).set('#');
+    cout << myscreen.get() << endl;
+
+//    myscreen.some_member();
+//    myscreen.some_member();
+
     return 0;
 }
