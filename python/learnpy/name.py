88ef2c06dbead5d6339d4d292c11640225b26517
Ye Hu <yehuwuhan@gmail.com>
2018-02-08 22:39:49 +0800

    add some py files

---
+++ python/learnpy/name.py
@@ -0,0 +1,14 @@
+# 字符串
+# "" or '' 中包含的字符串
+
+name = "ada lovelace"
+# 以首字母大写的方式显示每个单词
+print(name.title())
+# 说明字符串本身没变
+print(name)
+
+name = "Ada Lovelace"
+# 全部大写显示
+print(name.upper())
+# 全部小写显示
+print(name.lower())
